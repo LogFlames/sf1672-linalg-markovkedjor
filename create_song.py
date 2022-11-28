dbcd2ff09de0677a162fa27a68b8492a97b06035
Elias Lundell <elilun03@gmail.com>
2022-11-28 16:26:32 +0100

    linebreaks

--- create_song.py
+++ create_song.py
@@ -18,18 +18,27 @@ def main():
     while (start_word := input("What word do you want to start the song on?: ").lower()) not in unique_sorted_words:
         print(f"The word '{start_word}' does not occur in the dataset, please try again.")
 
+    print(start_word, end = " ")
+
     current_word_vector = np.zeros(len(unique_sorted_words))
     current_word_vector[unique_sorted_words.index(start_word)] = 1
 
+    i = 0
     while True:
         next_word_index = get_next_word(current_word_vector, probability_matrix)
 
         if unique_sorted_words[next_word_index] == "slutslutslut":
             break
-        print(unique_sorted_words[next_word_index])
+
+        print(unique_sorted_words[next_word_index], end = " ")
+        i += 1
+        if i % 10 == 0:
+            print()
 
         current_word_vector = np.zeros(len(unique_sorted_words))
         current_word_vector[next_word_index] = 1
+
+    print()
 
 if __name__ == "__main__":
     main()
