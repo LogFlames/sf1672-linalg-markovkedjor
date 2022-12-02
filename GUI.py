3b256ae14b052c77e173f87dbef2e3e911950155
Ismael Parada <ismaelp@kth.se>
2022-12-02 13:54:02 +0100

    GUI update

--- GUI.py
+++ GUI.py
@@ -12,11 +12,7 @@ class Window():
 
         self.entered_phrase = tk.StringVar()
 
-<<<<<<< HEAD
-        self.l1 = tk.Label( self.root, text="Skriv hur du vill att sången ska börja")
-=======
         self.l1 = tk.Label(self.root, text="Skriv hur du vill att sången ska börja")
->>>>>>> 966866e7afb0bf31584723910f433c33cfdac64f
         self.l1.pack(side="top", pady=(15, 0))
         self.e1 = tk.Entry(self.root, textvariable=self.entered_phrase, justify="center")
         self.e1.pack(side="top", pady=(15, 20))
@@ -26,13 +22,8 @@ class Window():
         self.e1.bind("<Return>", self.return_pressed)
 
     def return_pressed(self, _):
-<<<<<<< HEAD
-        lyrics = generate_song(self.entered_phrase)
-=======
-        # Funkar inte eftersom create_song inte funkar så här :)
-        #lyrics = create_song(self.entered_phrase)
->>>>>>> 966866e7afb0bf31584723910f433c33cfdac64f
-        self.t1.insert(tk.INSERT, "lyrics")
+        lyrics = create_song.generate_song(self.entered_phrase)
+        self.t1.insert(tk.INSERT, lyrics)
 
 
 def main():
